6dc7d4d326df74ce4cbb4423cd006bd5d4108787
kandamashiro <641047675@qq.com>
2017-05-07 23:59:43 +0800

    completed login page

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,49 @@
-var process = require('child_process');
+// var process = require('child_process');
 //直接调用命令
 
-var child = process.exec('phantomjs ' + __dirname  + '/../shellCmd/login.js',
-  function (error, stdout, stderr) {
-    console.log(stdout);
-    if (error !== null) {
-      console.log('exec error:' + error);
-    }
-  });
+// var child = process.exec('phantomjs ' + __dirname  + '/../shellCmd/login.js',
+//   function (error, stdout, stderr) {
+//     console.log(stdout);
+//     if (error !== null) {
+//       console.log('exec error:' + error);
+//     }
+//   });
 
-child.on('message', function(m) {
-  console.log('main listen: ' + m);
+var $ = require('jquery');
+var path = require('path');
+var phantomjs = require('phantomjs');
+var spawn = require('child_process').spawn;
+
+var childArgs = [
+  path.join(__dirname, '/../shellCmd/login.js'),
+];
+var child = spawn(phantomjs.path, childArgs);
+
+child.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+  if (data.indexOf('out_data:') > -1) {
+    dispose(data.replace('out_data:'));
+  }
 });
+
+child.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+child.on('close', function (code) {
+  console.log('child process exited with code ' + code);
+});
+
+
+//事件绑定 暂时放这
+$('#jsLogin').on('submit', function() {
+  alert(1);
+});
+
+function dispose(signal) {
+  switch(signal) {
+    case 'NO_LOGIN':
+      $('#jsLogin').removeClass('hidden');
+      break;
+  }
+}
--- app/shellCmd/login.js
+++ app/shellCmd/login.js
@@ -1,24 +1,86 @@
 /**
  * Created by XiaMi on 2017/4/17.
  */
+var webpage = require('webpage');
+var page = webpage.create();//创建webpage对象
 
-var page = require('webpage').create();//创建webpage对象
-var sys = require('system');//创建system对象
+var fs = require('fs');
+var CookieJar = "cookiejar.json";
+
+page.onUrlChanged = function(targetUrl) {
+  console.log('out_data:NO_LOGIN');
+  page.close();
+  login();
+};
+
+page.onResourceReceived = function(response) {
+  fs.write(CookieJar, JSON.stringify(phantom.cookies), "w");
+};
+
+if(fs.isFile(CookieJar)) {
+  Array.prototype.forEach.call(JSON.parse(fs.read(CookieJar)), function(x){
+    phantom.addCookie(x);
+  });
+}
 
 
-page.open('https://passport.bilibili.com/login', function(status) {
-  if (status !== 'success') {
-    console.log('Fail to load the page!');
-    phantom.exit();
-  } else {
-    page.injectJs(page.libraryPath + '/../lib/jquery.js');
-    // page.injectJs(page.libraryPath + '/../lib/jquery.js', function() {
-    //   page.evaluate(function() {
-    //     $("button").click();
-    //   });
-    //   phantom.exit()
-    // });
-    page.render('test.png');
-    phantom.exit()
-  }
+// page.open('https://passport.bilibili.com/login', function(status) {
+page.open('http://member.bilibili.com/v/video/submit.html', function(status) {
+
 });
+
+// page.open('http://member.bilibili.com/v/video/submit.html', function(status) {
+//   // this assumes that when you are not logged in, the server replies with a 303
+//   console.log(JSON.stringify(pageResponses['http://space.bilibili.com/ajax/member/MyInfo']));
+//   if(pageResponses['http://member.bilibili.com/x/web/archive/pre'] == 302) {
+//     console.log('not logged');
+//     //attempt login
+//     //assuming a resourceRequested event is fired the cookies will be written to the jar and on your next load of the script they will be found and used
+//   } else {
+//     console.log('logged');
+//   }
+// });
+
+function login() {
+  var page = webpage.create();
+
+  // page.onLoadFinished =
+  // };
+
+  page.open('https://passport.bilibili.com/login', function(status) {
+    if (status !== 'success') {
+      console.log('Fail to load the page!');
+      phantom.exit();
+    } else {
+      console.log('进入登录页面');
+      var injectStatus = page.injectJs(page.libraryPath + '/../lib/jquery-1.11.3.min.js');
+
+      var imgObj = page.evaluate(function () {
+        $("#vdCodeTxt").val(123).focus().click();
+
+        var $captchaImg = $('#captchaImg');
+        var offset = $captchaImg.offset();
+        var height = $captchaImg.height();
+        var width = $captchaImg.width();
+
+        // console.log(1);
+        return {
+          top: offset.top,
+          left: offset.left,
+          height: height,
+          width: width
+        };
+      });
+      console.log('获取验证码位置高度：' + JSON.stringify(imgObj));
+      // page.clipRect = { top: 0, left: 0, width: 1024, height: 768 };
+      setTimeout(function () {
+        console.log('获取验证码');
+        page.render('all.png');
+        page.clipRect = imgObj;
+        page.render('test.png');
+        // console.log(injectStatus);
+        phantom.exit()
+      }, 2000);
+    }
+  });
+}
